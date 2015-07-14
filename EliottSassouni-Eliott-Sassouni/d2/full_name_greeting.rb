9376065d61368165859649558a7c773f17f1f03c
Eliott-Sassouni <eliott.sassouni@gmail.com>
2015-07-14 15:23:28 +0200

    Exercises 2B Full Name Greeting

---
+++ EliottSassouni-Eliott-Sassouni/d2/full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts "What is you first name?"
+puts first_name = gets.strip
+puts "What is your middle name?"
+puts middle_name = gets.strip
+puts "What is your last name?"
+puts last_name = gets.strip
+
+puts "Hello, #{first_name} #{middle_name} #{last_name}!"
