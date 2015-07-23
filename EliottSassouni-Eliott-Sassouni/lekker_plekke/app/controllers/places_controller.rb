da6737dc1b75b118edb3d250731702167a719ad1
Aaron Fuchs <aaronfuchs100@gmail.com>
2015-07-23 18:14:55 +0200

    Added the Edit button

--- EliottSassouni-Eliott-Sassouni/lekker_plekke/app/controllers/places_controller.rb
+++ EliottSassouni-Eliott-Sassouni/lekker_plekke/app/controllers/places_controller.rb
@@ -1,2 +1,44 @@
 class PlacesController < ApplicationController
+	def index
+		@places = Place.all.order(rating: :asc)
+	end
+
+	def show
+		@place = Place.find(params[:id])
+	end
+
+	def new
+		@place = Place.new
+	end
+
+	def create
+		@place = Place.new(place_params)
+
+		@place.save
+		if @place.save
+			redirect_to place_path(@place)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@place = Place.new(place_params)
+	end
+
+	def update
+		@place = Place.find(params[:id])
+
+		if @place.update(place_params)
+			redirect_to place_path(@place)
+		else
+			render :edit
+		end
+	end
+
+private
+
+	def place_params
+		params.require(:place).permit(:name, :rating, :address)
+	end
 end
