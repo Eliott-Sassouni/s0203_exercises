cb9229880e46e0731a638eae848d848e2db36a2e
Aaron Fuchs <aaronfuchs100@gmail.com>
2015-07-23 17:03:53 +0200

    Lekker PLekke initial commit

---
+++ EliottSassouni-Eliott-Sassouni/lekker_plekke/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Place.create! name: "Lion's Head", rating: 6, address: "Lion's Head Table Mountain National Park, Signal Hill, Cape Town"
+
+Place.create! name: "Cape Point", rating: 10, address: "Cape Point Table Mountain National Park, Cape Peninsula, Cape Town"
+
+Place.create! name: "Truth Cafe", rating: 9, address: "36 Buitenkant St, Cape Town City Centre, Cape Town"
+
+Place.create! name: "Dizzy's", rating: 5, address: "41, The Drive, Camps Bay Dr, Cape Town"
+
+Place.create! name: "Boulders Beach", rating: 7, address: "1 Kleintuin Road, Simon's Town, Cape Town, 7995"
